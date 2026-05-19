434b72568c9e8f1c9d2ceb155062f5b64971a53f
Eryc <erycfreire.pro@gmail.com>
2026-05-19 20:55:30 -0300

    Crriando a função deposito ETH com prazo de bloqueio em dias

--- eth-vault/contracts/EthVault.sol
+++ eth-vault/contracts/EthVault.sol
@@ -1,6 +1,25 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
-pragma solidity ^0.8.23;
+pragma solidity ^0.8.30;
 
 contract EthVault {
-    
+    struct Deposit {
+        uint amount;
+        uint unlockTime;
+    }
+
+    mapping (address => Deposit) public deposits;
+
+    //Deposito ETH com prazo de bloqueio em dias
+    function deposit (uint lockDays) external payable{
+        require (msg.value > 0, "Valor deve ser maior do que zero.");
+        require (deposits[msg.sender].amount == 0, "Voce ja tem um deposito ativo.");
+        require (lockDays > 0, "Prazo deve ser maior que zero.");
+
+        uint unlockTime = block.timestamp + (lockDays * 1 days);
+
+        deposits[msg.sender] = Deposit({
+            amount: msg.value,
+            unlockTime: unlockTime
+        });
+    }
 }
