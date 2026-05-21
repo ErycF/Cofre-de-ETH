d7647bfa68b4199e78a1fc10ca28fa58ae6dc971
Eryc <erycfreire.pro@gmail.com>
2026-05-21 19:54:33 -0300

    Atualização do função withdraw

--- eth-vault/contracts/EthVault.sol
+++ eth-vault/contracts/EthVault.sol
@@ -9,8 +9,11 @@ contract EthVault {
 
     mapping (address => Deposit) public deposits;
 
+    event Deposited(address indexed user, uint256 amount, uint256 unlockTime);
+    event Withdraw(address indexed user, uint256 amount);
+
     //Deposito ETH com prazo de bloqueio em dias
-    function deposit (uint lockMinutes) external payable{
+    function deposit(uint lockMinutes) external payable{
         require (msg.value > 0, "Valor deve ser maior do que zero.");
         require (deposits[msg.sender].amount == 0, "Voce ja tem um deposito ativo.");
         require (lockMinutes > 0, "Prazo deve ser maior que zero.");
@@ -21,5 +24,14 @@ contract EthVault {
             amount: msg.value,
             unlockTime: unlockTime
         });
+
+        emit Deposited(msg.sender, msg.value, unlockTime);
+    }
+    //Saca o ETH se o prazo já passou
+    function withdraw() external{
+        Deposit memory userDeposit = deposits[msg.sender];
+
+        require(userDeposit.amount > 0, "Nenhum deposito encontrado.");
+        require(block.timestamp >= userDeposit.unlockTime, "Prazo ainda nao acabou.");
     }
 }
